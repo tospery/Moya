6d164839b0999393bab670452295d055481e2804
YangJianxiang <tospery@gmail.com>
2023-01-21 16:15:21 +0800

    15.0.0-v4

---
+++ Example/Moya-Hi/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Moya-Hi
+//
+//  Created by YangJianxiang on 01/21/2023.
+//  Copyright (c) 2023 YangJianxiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/Moya-Hi/Moya-Hi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Moya_Hi : NSObject
+@end
+@implementation PodsDummy_Moya_Hi
+@end
